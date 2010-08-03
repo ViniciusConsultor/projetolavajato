d1143bd7fceae99f2ef303ab24ff6a9eb53629e4
henry.charriere <henry.charriere@259fd912-0795-d970-ad9d-e0d6b527d5ce>
2010-08-03 19:27:55 +0000

    consulta vendas e backup da base

---
+++ Lavajato/data/Vendas.sql
@@ -0,0 +1,17 @@
+select distinct min(s.servicoid) as [O.S. Inicial], max(s.servicoid) as [O.S. Final],
+(select count(servico.cancelado) from servico where  servico.cancelado = 1 and s.servicoid = servico.servicoid) as [O.S. Cancelado],
+sum(s.Total) as [Total Vendas], sum(s.Desconto) as [Total Desconto],
+isnull((select sum(servico.total) from servico inner join formapagamento fp on fp.formapagamentoid = servico.formapagamentoid where servico.formapagamentoid = 1 and s.servicoid = servico.servicoid),0) as [Dinheiro],
+isnull((select sum(servico.total) from servico inner join formapagamento fp on fp.formapagamentoid = servico.formapagamentoid where servico.formapagamentoid = 2 and s.servicoid = servico.servicoid),0)  as [Visa Debito],
+isnull((select sum(servico.total) from servico inner join formapagamento fp on fp.formapagamentoid = servico.formapagamentoid where servico.formapagamentoid = 3 and s.servicoid = servico.servicoid),0)  as [Visa Credito],
+isnull((select sum(servico.total) from servico inner join formapagamento fp on fp.formapagamentoid = servico.formapagamentoid where servico.formapagamentoid = 4 and s.servicoid = servico.servicoid),0) as [Master Debito],
+isnull((select sum(servico.total) from servico inner join formapagamento fp on fp.formapagamentoid = servico.formapagamentoid where servico.formapagamentoid = 5 and s.servicoid = servico.servicoid),0)  as [Master Debito]
+from servico s
+--where s.entrada >= '2010-06-01 00:00:00.000' and s.entrada <= '2010-10-15 23:59:59.999'
+group by s.ServicoID
+	
+
+
+
+--select * from servico-- where entrada = '2010-06-10 23:04:43.000'
+--select * from formapagamento
