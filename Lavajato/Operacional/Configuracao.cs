404da79d059bfb01e071350fbddce5b993aacdb0
henry.charriere <henry.charriere@259fd912-0795-d970-ad9d-e0d6b527d5ce>
2010-08-15 04:37:39 +0000

    incluido metodos para impressao de recibos

--- Lavajato/Operacional/Configuracao.cs
+++ Lavajato/Operacional/Configuracao.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using HenryCorporation.Lavajato.DomainModel;
 
 namespace HenryCorporation.Lavajato.Operacional
 {
-    public static class Configuracao
+    public class Configuracao
     {
-        static Configuracao()
+        public Configuracao()
         {
 
         }
@@ -84,6 +87,65 @@ namespace HenryCorporation.Lavajato.Operacional
             string dia = hora.Day.ToString().Length == 1 ? "0" + hora.Day.ToString() : hora.Day.ToString();
             string mes = hora.Month.ToString().Length == 1 ? "0" + hora.Month.ToString() : hora.Month.ToString();
             return dia + "/" + mes + "/" + hora.Year;
+        }
+
+        private Servico servico;
+        private  PrintDocument recibo;
+        public int EmiteRecibo(Servico servico)
+        {
+            this.servico = servico;
+            if (this.servico.ID == 0)
+                return 0;
+
+            recibo = new PrintDocument();
+            recibo.PrintPage += new PrintPageEventHandler(this.recibo_PrintPage);
+            recibo.Print();
+
+            return 1;
+        }
+        
+        private void recibo_PrintPage(object sender, PrintPageEventArgs ev)
+        {
+            string enter = "\n";
+            Pen myPen = new Pen(Brushes.Black);
+            Point pt1 = new Point(30, 53);
+            Font myFont1 = new Font("Arial", 9);
+
+            StringBuilder strCabecalho = new StringBuilder();
+            strCabecalho.Append("LAVEVIP LAVAJATO" + enter);
+            strCabecalho.Append("Rua da Bahia, 2244, Lourdes" + enter);
+            strCabecalho.Append("Minas Tenis Clube-Piso 1" + enter);
+            strCabecalho.Append("(31)xxxx-xxxx" + enter);
+            strCabecalho.Append("----------------------------------------------" + enter);
+            strCabecalho.Append("Placa: " + servico.Cliente.Placa + "  Veículo:" + servico.Cliente.Veiculo + "  Cor:" + servico.Cliente.Cor + "" + enter);
+            strCabecalho.Append("Nome:  " + servico.Cliente.Nome + "      Fone:" + servico.Cliente.Telefone + "" + enter);
+            strCabecalho.Append("Entrada:  " + servico.Entrada + "   Saida:" + servico.Saida.Hour.ToString() + "" + enter);
+            strCabecalho.Append("----------------------------------------------" + enter);
+            strCabecalho.Append("SERVICO             QTDE.     VALOR   TOTAL" + enter);
+
+            string desc = "";
+            foreach (var item in servico.ServicoItem)
+            {
+                if (item.Produto.Descricao.Length >= 16)
+                {
+                    desc = item.Produto.Descricao;
+                }
+                else
+                {
+                    for (int i = 0; i < 16 - item.Produto.Descricao.Length; i++)
+                    {
+                        desc += " ";
+                    }
+                }
+
+                string qtde = item.Quantidade.ToString();
+                string valUni = item.Produto.ValorUnitario.ToString("C").Replace("R$", "");
+                string tot = (item.Produto.ValorUnitario * item.Quantidade).ToString("C").Replace("R$", "");
+                strCabecalho.Append(desc + qtde + "       " + valUni + tot + enter);
+            }
+
+            ev.Graphics.DrawString(strCabecalho.ToString(), myFont1, Brushes.Black, 30, 30);
+            ev.HasMorePages = false;
         }
 
 
